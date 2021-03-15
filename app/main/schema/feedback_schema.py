b08f41c5e95e18e2c0e3fede9eb94a85dd42df4e
Leyla Horne <lhorne@gavant.com>
2021-03-15 09:48:20 -0400

    Added reporting users name to github tickets

--- app/main/schema/feedback_schema.py
+++ app/main/schema/feedback_schema.py
@@ -12,4 +12,5 @@ class FeedbackSchema(BaseSchema):
     url = fields.Str(attribute="url", required=True, validate=Length(max=256))
     feedback_type = fields.Str(attribute="feedback_type", required=True, validate=OneOf(["FEEDBACK", "ISSUE"]))
     browser = fields.Str(attribute="browser", required=True, validate=Length(max=128))
+    reporter_name = fields.Str(attribute="reporter_name", required=True, validate=Length(max=128))
 
