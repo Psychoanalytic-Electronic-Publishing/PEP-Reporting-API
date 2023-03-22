6ddc02d4996395e9b1528aa90e51cf53338cf76b
Jordan Allen <jordan@jordanallen.org>
2023-03-22 17:46:38 +0000

    Contain env vars inside COnfig class
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- app/main/config.py
+++ app/main/config.py
@@ -1,7 +1,12 @@
 import os
 
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
-GITHUB_REPO = os.environ.get("GITHUB_REPO")
-GITHUB_LABELS =  os.environ.get("GITHUB_LABELS").split(",") if os.environ.get("GITHUB_LABELS") else []
-GITHUB_ASSIGNEES_FEEDBACK = os.environ.get("GITHUB_ASSIGNEES_FEEDBACK").split(",") if os.environ.get("GITHUB_ASSIGNEES_FEEDBACK") else []
-GITHUB_ASSIGNEES_DATAERROR = os.environ.get("GITHUB_ASSIGNEES_DATAERROR").split(",") if os.environ.get("GITHUB_ASSIGNEES_DATAERROR") else []
+
+class Config:
+    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+    GITHUB_REPO = os.environ.get("GITHUB_REPO")
+    GITHUB_LABELS = os.environ.get("GITHUB_LABELS").split(
+        ",") if os.environ.get("GITHUB_LABELS") else []
+    GITHUB_ASSIGNEES_FEEDBACK = os.environ.get("GITHUB_ASSIGNEES_FEEDBACK").split(
+        ",") if os.environ.get("GITHUB_ASSIGNEES_FEEDBACK") else []
+    GITHUB_ASSIGNEES_DATAERROR = os.environ.get("GITHUB_ASSIGNEES_DATAERROR").split(
+        ",") if os.environ.get("GITHUB_ASSIGNEES_DATAERROR") else []
