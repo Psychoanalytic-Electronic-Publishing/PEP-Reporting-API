743f462f2b3cc20c996a2e12804d9b1d9791a086
Jordan Allen <jordan@jordanallen.org>
2023-03-24 00:31:43 +0000

    Remove test lambda
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/staging/lambda.tf
+++ infra/staging/lambda.tf
@@ -46,27 +46,3 @@ module "feedback_lambda" {
     "GITHUB_TOKEN"               = "..."
   }
 }
-module "test_lambda" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "4.9.0"
-
-  function_name           = "${var.stack_name}-test-handler-${var.env}"
-  source_path             = "../../app"
-  handler                 = "main/controller/feedback_controller.handler"
-  runtime                 = "python3.8"
-  timeout                 = 29
-  ignore_source_code_hash = true
-
-  tags = {
-    stage = var.env
-    stack = var.stack_name
-  }
-
-  environment_variables = {
-    "GITHUB_ASSIGNEES_DATAERROR" = "jordanallen-dev"
-    "GITHUB_ASSIGNEES_FEEDBACK"  = "jordanallen-dev"
-    "GITHUB_LABELS"              = "Client User Issue Reported"
-    "GITHUB_REPO"                = "jordanallen-dev/pep-test"
-    "GITHUB_TOKEN"               = "..."
-  }
-}
