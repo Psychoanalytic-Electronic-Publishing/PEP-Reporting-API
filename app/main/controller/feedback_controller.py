364f97d7861bdcc18ecfc7fba95138932e08dfc2
Jordan Allen <jordan@jordanallen.org>
2023-03-28 16:59:31 +0100

    Return error and success response in format for feedback
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- app/main/controller/feedback_controller.py
+++ app/main/controller/feedback_controller.py
@@ -1,7 +1,7 @@
 from main.service.feedback_service import FeedbackService
 from main.schema.feedback_schema import FeedbackSchema
 from marshmallow import EXCLUDE
-
+import json
 
 create_schema = FeedbackSchema(unknown=EXCLUDE)
 
@@ -9,5 +9,19 @@ create_schema = FeedbackSchema(unknown=EXCLUDE)
 def handler(event, context):
     print(event)
 
-    data = create_schema.loads(event['body'])
-    return FeedbackService.create_issue(data=data)
+    try:
+        data = create_schema.loads(event['body'])
+        return FeedbackService.create_issue(data=data)
+
+        return {
+            "statusCode": 200,
+            "body": resp
+        }
+
+    except Exception as e:
+        print(e)
+
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": str(e)})
+        }
