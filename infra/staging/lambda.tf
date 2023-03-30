6edb57dd12f7897697674d5abda2b9a216451352
Jordan Allen <jordan@jordanallen.org>
2023-03-30 18:59:41 +0100

    Uncomment feedback lambda
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/staging/lambda.tf
+++ infra/staging/lambda.tf
@@ -54,49 +54,49 @@ resource "aws_lambda_permission" "allow_api_data_error" {
 }
 
 
-# module "feedback_lambda" {
-#   source  = "terraform-aws-modules/lambda/aws"
-#   version = "4.9.0"
+module "feedback_lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "4.9.0"
 
-#   function_name = "${var.stack_name}-feedback-handler-${var.env}"
-#   source_path = [
-#     {
-#       path = "../../app",
-#       commands = [
-#         "pip install --platform manylinux2014_x86_64 --implementation cp --python 3.8 --only-binary=:all: --upgrade -t . cryptography",
-#         "pip install -r requirements.txt -t .",
-#         ":zip"
-#       ]
-#   }]
-#   handler                 = "main/controller/feedback_controller.handler"
-#   runtime                 = "python3.8"
-#   timeout                 = 29
-#   ignore_source_code_hash = true
-
-
-#   tags = {
-#     stage = var.env
-#     stack = var.stack_name
-#   }
-
-#   environment_variables = {
-#     "GITHUB_ASSIGNEES"       = var.github_assignees_feedback
-#     "GITHUB_LABELS"          = var.github_labels
-#     "GITHUB_OWNER"           = var.github_owner
-#     "GITHUB_REPO"            = var.github_repo
-#     "GITHUB_PRIVATE_KEY"     = var.github_private_key
-#     "GITHUB_APP_ID"          = var.github_app_id
-#     "GITHUB_INSTALLATION_ID" = var.github_installation_id
-#     "CORS_ORIGIN"            = var.cors_origin
-#   }
-# }
+  function_name = "${var.stack_name}-feedback-handler-${var.env}"
+  source_path = [
+    {
+      path = "../../app",
+      commands = [
+        "pip install --platform manylinux2014_x86_64 --implementation cp --python 3.8 --only-binary=:all: --upgrade -t . cryptography",
+        "pip install -r requirements.txt -t .",
+        ":zip"
+      ]
+  }]
+  handler                 = "main/controller/feedback_controller.handler"
+  runtime                 = "python3.8"
+  timeout                 = 29
+  ignore_source_code_hash = true
 
 
-# resource "aws_lambda_permission" "allow_api_data_feedback" {
-#   statement_id  = "${var.stack_name}-allow-api-feedback-${var.env}"
-#   action        = "lambda:InvokeFunction"
-#   function_name = module.feedback_lambda.lambda_function_name
-#   principal     = "apigateway.amazonaws.com"
-#   source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/*"
-# }
+  tags = {
+    stage = var.env
+    stack = var.stack_name
+  }
+
+  environment_variables = {
+    "GITHUB_ASSIGNEES"       = var.github_assignees_feedback
+    "GITHUB_LABELS"          = var.github_labels
+    "GITHUB_OWNER"           = var.github_owner
+    "GITHUB_REPO"            = var.github_repo
+    "GITHUB_PRIVATE_KEY"     = var.github_private_key
+    "GITHUB_APP_ID"          = var.github_app_id
+    "GITHUB_INSTALLATION_ID" = var.github_installation_id
+    "CORS_ORIGIN"            = var.cors_origin
+  }
+}
+
+
+resource "aws_lambda_permission" "allow_api_data_feedback" {
+  statement_id  = "${var.stack_name}-allow-api-feedback-${var.env}"
+  action        = "lambda:InvokeFunction"
+  function_name = module.feedback_lambda.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/*"
+}
 
