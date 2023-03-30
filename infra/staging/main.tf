4c5ede81862894d9b84a34a3b9913eba6bf3c22f
Jordan Allen <jordan@jordanallen.org>
2023-03-30 20:44:45 +0100

    Pass variables into module
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/staging/main.tf
+++ infra/staging/main.tf
@@ -19,4 +19,19 @@ provider "aws" {
 
 module "reporting_api" {
   source = "../modules/reporting"
+
+  env                         = var.env
+  stack_name                  = var.stack_name
+  github_assignees_data_error = var.github_assignees_data_error
+  github_assignees_feedback   = var.github_assignees_feedback
+  github_labels               = var.github_labels
+  github_private_key          = var.github_private_key
+  github_app_id               = var.github_app_id
+  github_installation_id      = var.github_installation_id
+  github_owner                = var.github_owner
+  github_repo                 = var.github_repo
+  root_domain                 = var.root_domain
+  cors_origin                 = var.cors_origin
+  api_domain                  = var.api_domain
+
 }
