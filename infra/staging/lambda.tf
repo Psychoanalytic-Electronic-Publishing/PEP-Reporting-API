60cca07026457560de634d8c7bbbb816e3de83c1
Jordan Allen <jordan@jordanallen.org>
2023-03-30 19:42:49 +0100

    Revert split packaging into modules
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/staging/lambda.tf
+++ infra/staging/lambda.tf
@@ -1,8 +1,8 @@
-module "package_error" {
-
+module "data_error_lambda" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "2.31.0"
+  version = "4.9.0"
 
+  function_name = "${var.stack_name}-data-error-handler-${var.env}"
   source_path = [
     {
       path = "../../app",
@@ -12,23 +12,10 @@ module "package_error" {
         ":zip"
       ]
   }]
-  runtime                  = "python3.8"
-  create_function          = false
-  recreate_missing_package = true
-}
-
-module "data_error_lambda" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "4.9.0"
-
-  function_name          = "${var.stack_name}-data-error-handler-${var.env}"
-  local_existing_package = module.package_error.local_filename
-  create_package         = false
-  publish                = true
-
-  handler = "main/controller/data_error_controller.handler"
-  runtime = "python3.8"
-  timeout = 29
+  handler                 = "main/controller/data_error_controller.handler"
+  runtime                 = "python3.8"
+  timeout                 = 29
+  ignore_source_code_hash = true
 
   tags = {
     stage = var.env
@@ -54,61 +41,12 @@ resource "aws_lambda_permission" "allow_api_data_error" {
   source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/*"
 }
 
-module "package_feedback" {
-
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "2.31.0"
-
-  source_path = [
-    {
-      path = "../../app",
-      commands = [
-        "pip install --platform manylinux2014_x86_64 --implementation cp --python 3.8 --only-binary=:all: --upgrade -t . cryptography",
-        "pip install -r requirements.txt -t .",
-        ":zip"
-      ]
-  }]
-  runtime                  = "python3.8"
-  create_function          = false
-  recreate_missing_package = true
-}
-
 
 module "feedback_lambda" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "4.9.0"
 
-  function_name          = "${var.stack_name}-feedback-handler-${var.env}"
-  local_existing_package = module.package_feedback.local_filename
-  create_package         = false
-  publish                = true
-
-  handler = "main/controller/feedback_controller.handler"
-  runtime = "python3.8"
-  timeout = 29
-
-  tags = {
-    stage = var.env
-    stack = var.stack_name
-  }
-
-  environment_variables = {
-    "GITHUB_ASSIGNEES"       = var.github_assignees_feedback
-    "GITHUB_LABELS"          = var.github_labels
-    "GITHUB_OWNER"           = var.github_owner
-    "GITHUB_REPO"            = var.github_repo
-    "GITHUB_PRIVATE_KEY"     = var.github_private_key
-    "GITHUB_APP_ID"          = var.github_app_id
-    "GITHUB_INSTALLATION_ID" = var.github_installation_id
-    "CORS_ORIGIN"            = var.cors_origin
-  }
-}
-
-module "package_test" {
-
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "2.31.0"
-
+  function_name = "${var.stack_name}-feedback-handler-${var.env}"
   source_path = [
     {
       path = "../../app",
@@ -118,24 +56,11 @@ module "package_test" {
         ":zip"
       ]
   }]
-  runtime                  = "python3.8"
-  create_function          = false
-  recreate_missing_package = true
-}
+  handler                 = "main/controller/feedback_controller.handler"
+  runtime                 = "python3.8"
+  timeout                 = 29
+  ignore_source_code_hash = true
 
-
-module "test_lambda" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "4.9.0"
-
-  function_name          = "${var.stack_name}-test-${var.env}"
-  local_existing_package = module.package_test.local_filename
-  create_package         = false
-  publish                = true
-
-  handler = "main/controller/feedback_controller.handler"
-  runtime = "python3.8"
-  timeout = 29
 
   tags = {
     stage = var.env
@@ -153,7 +78,6 @@ module "test_lambda" {
     "CORS_ORIGIN"            = var.cors_origin
   }
 }
-
 
 
 resource "aws_lambda_permission" "allow_api_data_feedback" {
