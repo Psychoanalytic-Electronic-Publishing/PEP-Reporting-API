8e3e9d61caf22c6d9660608de592c5daaee346ca
Jordan Allen <jordan@jordanallen.org>
2023-03-30 19:09:24 +0100

    Add test lambda
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/staging/lambda.tf
+++ infra/staging/lambda.tf
@@ -83,6 +83,37 @@ module "feedback_lambda" {
   }
 }
 
+module "test_lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "4.9.0"
+
+  function_name          = "${var.stack_name}-test-${var.env}"
+  local_existing_package = module.package.local_filename
+  create_package         = false
+  publish                = true
+
+  handler = "main/controller/feedback_controller.handler"
+  runtime = "python3.8"
+  timeout = 29
+
+  tags = {
+    stage = var.env
+    stack = var.stack_name
+  }
+
+  environment_variables = {
+    "GITHUB_ASSIGNEES"       = var.github_assignees_feedback
+    "GITHUB_LABELS"          = var.github_labels
+    "GITHUB_OWNER"           = var.github_owner
+    "GITHUB_REPO"            = var.github_repo
+    "GITHUB_PRIVATE_KEY"     = var.github_private_key
+    "GITHUB_APP_ID"          = var.github_app_id
+    "GITHUB_INSTALLATION_ID" = var.github_installation_id
+    "CORS_ORIGIN"            = var.cors_origin
+  }
+}
+
+
 
 resource "aws_lambda_permission" "allow_api_data_feedback" {
   statement_id  = "${var.stack_name}-allow-api-feedback-${var.env}"
